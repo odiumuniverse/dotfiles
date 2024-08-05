d384bfccc6f05d287b9eb3873d586e4eef994fb3
Azamat Ishbaev <isbaevazamat4@gmail.com>
2024-08-06 00:50:06 +0300

    feat: update tmux and ref nvim

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 -- config.color_scheme = 'Bluloco Light (Gogh)'
-config.color_scheme = 'Catppuccin Latte'
+-- config.color_scheme = 'Catppuccin Latte'
 
 
 -- config.color_scheme = 'Google Light (Gogh)'
--- config.color_scheme = 'Google (light) (terminal.sexy)'
+config.color_scheme = 'Google (light) (terminal.sexy)'
 config.font = wezterm.font("RobotoMono Nerd Font")
 config.font_size = 17
 
