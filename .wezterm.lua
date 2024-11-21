80e70c1d0767f073ddd29445625a1098527091a2
Azamat Ishbaev <isbaevazamat4@gmail.com>
2024-11-22 00:49:53 +0300

    feat(font/wezterm): firacode instead robotomono

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,10 +4,10 @@ local config = wezterm.config_builder()
 -- config.color_scheme = 'Bluloco Light (Gogh)'
 -- config.color_scheme = 'Catppuccin Latte'
 
-
 -- config.color_scheme = 'Google Light (Gogh)'
-config.color_scheme = 'Google (light) (terminal.sexy)'
-config.font = wezterm.font("RobotoMono Nerd Font")
+config.color_scheme = "Google (light) (terminal.sexy)"
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+-- config.font = wezterm.font("RobotoMono Nerd Font")
 config.font_size = 17
 
 config.enable_tab_bar = false
