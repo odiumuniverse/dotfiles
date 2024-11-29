63ff38bef945dc191e05843757de02a5c074bbf6
Azamat Ishbaev <isbaevazamat4@gmail.com>
2024-11-29 10:44:50 +0300

    change(wezterm): background opacity now 0.9 instead 1

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
--- config.color_scheme = 'Bluloco Light (Gogh)'
--- config.color_scheme = 'Catppuccin Latte'
+-- config.color_scheme = "Bluloco Light (Gogh)"
+-- config.color_scheme = "Catppuccin Latte"
 
--- config.color_scheme = 'Google Light (Gogh)'
+-- config.color_scheme = "Google Light (Gogh)"
 config.color_scheme = "Google (light) (terminal.sexy)"
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 -- config.font = wezterm.font("RobotoMono Nerd Font")
@@ -13,7 +13,7 @@ config.font_size = 17
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 1
+config.window_background_opacity = 0.99
 config.macos_window_background_blur = 20
 
 return config
