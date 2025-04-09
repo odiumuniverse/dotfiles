52c3ad7fa4d05829ee61002650b1a48dd86c1881
Azamat Ishbaev <isbaevazamat4@gmail.com>
2025-04-09 21:34:38 +0300

    ref: return float terminal

---
+++ .config/nvim/lua/plugins/term.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      win = {
+        style = "terminal",
+        position = "float",
+        backdrop = 60,
+        height = 0.9,
+        width = 0.9,
+        zindex = 50,
+      },
+    },
+  },
+}
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,21 +1,20 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-config.colors = require("cyberdream")
 -- config.color_scheme = "Bluloco Light (Gogh)"
 -- config.color_scheme = "Catppuccin Latte"
 
 -- config.color_scheme = "Google Light (Gogh)"
--- config.color_scheme = "Google (light) (terminal.sexy)"
+config.color_scheme = "Google (light) (terminal.sexy)"
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 -- config.font = wezterm.font("RobotoMono Nerd Font")
 config.font_size = 17
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.7
+config.window_background_opacity = 1
 config.macos_window_background_blur = 30
-config.window_background_image = (os.getenv("WEZTERM_CONFIG_FILE") or ""):gsub("wezterm.lua", "tower1.png")
+-- config.window_background_image = (os.getenv("WEZTERM_CONFIG_FILE") or ""):gsub("wezterm.lua", "tower1.png")
 config.window_padding = {
 	left = 5,
 	right = 0,
